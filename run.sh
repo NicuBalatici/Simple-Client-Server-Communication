9402ac13e6e271fb457421bb1d9ec1dff19468aa
NicuBalatici <nicu.marian28@yahoo.com>
2025-05-15 14:50:04 +0300

    run.sh removed

--- run.sh
+++
@@ -1,7 +0,0 @@
-echo # Simple-Client-Server-Communication >> README.md
-git init
-git add README.md
-git commit -m first commit
-git branch -M main
-git remote add origin https://github.com/NicuBalatici/Simple-Client-Server-Communication.git
-git push -u origin main
